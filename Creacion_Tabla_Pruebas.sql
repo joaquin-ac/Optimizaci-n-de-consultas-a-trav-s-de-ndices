cb685630e8ec3ad0d8ad6e54cd1158650454e7a3
joaquin-ac <joaquin.acb36@gmail.com>
2023-11-13 21:38:57 -0300

    a

--- Creacion_Tabla_Pruebas.sql
+++ Creacion_Tabla_Pruebas.sql
@@ -1,17 +1,17 @@
 
 --Creacion tabla de prueba
-Create table gastoMillon	(
-						idgasto int identity,
-						idprovincia int,
-                         idlocalidad int,
-                         idconsorcio int, 
-					     periodo int,
-					     fechapago datetime,					     
-						 idtipogasto int,
-						 importe decimal (8,2),	
-						 Constraint FK_gastoMillon_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio) REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
-						 Constraint FK_gastoMillon_tipo FOREIGN KEY (idtipogasto) REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
-							)
+Create table gastoMillon(
+idgasto int identity,
+idprovincia int,
+idlocalidad int,
+idconsorcio int, 
+periodo int,
+fechapago datetime,					     
+idtipogasto int,
+importe decimal (8,2),	
+Constraint FK_gastoMillon_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio) REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
+Constraint FK_gastoMillon_tipo FOREIGN KEY (idtipogasto) REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
+)
 
 --Carga lote de prueba
 -- INICIO BUCLE
@@ -28,12 +28,12 @@ BEGIN
 
     insert into gastoMillon
     select	idprovincia,
-			idlocalidad,
-			idconsorcio,
-			periodo,
-			fechapago,
-			idtipogasto,
-			importe from gasto
+	idlocalidad,
+	idconsorcio,
+	periodo,
+	fechapago,
+	idtipogasto,
+	importe from gasto
 
     -- Incrementar el ID para pasar al siguiente registro
     SET @id = @id + 1;
