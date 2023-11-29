1e42d7d6822b58bd9a7499ec5f6a67eaa044c43a
joaquin-ac <joaquin.acb36@gmail.com>
2023-11-28 23:46:11 -0300

    arreglo orden

--- IntegraciónDeTemas.sql
+++ IntegraciónDeTemas.sql
@@ -2,12 +2,36 @@
 -- INTEGRACION Optimizacion de consultas - Vistas indexadas - Tipo de datos JSON
 ----------------------------------------------------------------------------------
 
+
+-- Creación de indice
+CREATE CLUSTERED INDEX IX_ClusteredIndex ON gastoMillon(idgasto)
+
+
 -- Agregar una columna JSON
 ALTER TABLE gastoMillon
 ADD datos_json NVARCHAR(MAX)
 
+
+-- Creación de la vista indexada con JSON
+CREATE VIEW VistaGastoConJSON AS
+(
+	SELECT 
+			idgasto, 
+			idprovincia, 
+			idlocalidad, 
+			idconsorcio, 
+			periodo, 
+			fechapago, 
+			importe,
+			datos_json
+		FROM gastoMillon
+		WHERE idgasto IN (999, 800, 7087)
+)
+GO
+
+
 -- Actualizar los datos a formato JSON
-UPDATE gastoMillon
+UPDATE VistaGastoConJSON
 SET datos_json = (
 	SELECT 
 			idgasto, 
@@ -23,26 +47,6 @@ SET datos_json = (
 	FOR JSON AUTO
 );
 
--- Creación de la vista indexada con JSON
-CREATE VIEW VistaGastoConJSON AS
-(
-	SELECT 
-			idgasto, 
-			idprovincia, 
-			idlocalidad, 
-			idconsorcio, 
-			periodo, 
-			fechapago, 
-			importe,
-			datos_json
-		FROM [dbo].[gastoMillon]
-		WHERE idgasto IN (999, 800, 7087)
-)
-GO
-
--- Creación de indice
-CREATE  CLUSTERED INDEX IX_VistaGastoConJSON ON VistaGastoConJSON(idgasto);
-GO
 
 -- Consulta utilizando datos JSON
 SELECT * FROM VistaGastoConJSON;
